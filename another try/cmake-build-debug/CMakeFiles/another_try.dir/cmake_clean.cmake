04f718c2c4e8b5011043a2b5e786c8dc654b3b7b
Peter Ngure <peter.ngure@outlook.com>
2021-02-26 23:56:20 +0100

    first commit

---
+++ another try/cmake-build-debug/CMakeFiles/another_try.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/another_try.dir/main.cpp.obj"
+  "another_try.exe"
+  "another_try.exe.manifest"
+  "another_try.lib"
+  "another_try.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/another_try.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ another try/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout<< "n ="<<std::endl;
+    std::cin>> n;
+    int array[n];// heap allocation stack allocation
+    for (int i = 0; i < n; ++i) {
+        std::cin>>array[i];
+    }
+    for (int j = 0; j < n; ++j) {
+        std::cout << array[j] <<" ";
+    }
+
+    return 0;
+}
